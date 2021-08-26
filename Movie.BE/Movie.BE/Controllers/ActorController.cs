ce14dad1fdb9064971816f3ea29941fdd84c14d6
Eduardo <eduardsalvadorgonzalez@gmail.com>
2021-08-26 14:43:08 -0500

    CH

--- Movie.BE/Movie.BE/Controllers/ActorController.cs
+++ Movie.BE/Movie.BE/Controllers/ActorController.cs
@@ -27,7 +27,7 @@ namespace Movie.BE.Controllers
         public ActorController(MoviesContext db)
         {
             _dal = new ActorDAL(db);
-            // RAMA ET1 DEV
+            // RAMA ET1 DEV BRANCS
         }
         /// <summary>
         /// Obtiene todos los actores de la pelicula
